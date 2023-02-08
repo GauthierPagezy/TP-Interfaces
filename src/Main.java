b0c94ded199392005f59681a7a4ee3953ba046c4
BuildTools <unconfigured@null.spigotmc.org>
2023-02-08 20:29:17 +0100

    ajout setSalaire() dans Professeur

--- src/Main.java
+++ src/Main.java
@@ -1,20 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-    /*Test passé
+    /*Test passé mais obselète car Humain Abstract
     Humain h1 = new Humain("Michel");
     System.out.println(h1.getNom());
     h1.meurt();
     */
 
-    //Test passé
+    /*Test passé
     Etudiant e1 = new Etudiant("Michel", 42.20);
     System.out.println(e1.getNom());
     e1.meurt();
     System.out.println(e1.getNote());
-
-
-
-
+    e1.setNom("dd");
+    */
 
     }
 }
--- src/Professeur.java
+++ src/Professeur.java
@@ -1,25 +1,20 @@
 
 
-public class Professeur extends Humain {
+public abstract class Professeur extends Humain {
     private double salaire;
 
     public Professeur(){
         super("Indéfini");
         this.salaire = 0;
     }
-    public Professeur(String nom){
-        super(nom);
-    }
-
+    public Professeur(String nom){super(nom);}
     public Professeur(String nom, double salaire) {
         super(nom);
         this.salaire = salaire;
     }
-
-    @Override
-    public void setNom(String nom){
-        this.nom = nom;
-    }
-
     public double getSalaire(){return this.salaire;}
+    public void setSalaire(double salaire){this.salaire=salaire;}
+    @Override
+    public void setNom(String nom){this.nom = nom;}
+
 }

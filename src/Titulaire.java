f23f5962beb41065a4b4c59afd13796326bbd5af
BuildTools <unconfigured@null.spigotmc.org>
2023-02-08 20:40:13 +0100

    ajout de Titulaire

---
+++ src/Titulaire.java
@@ -0,0 +1,24 @@
+public abstract class Titulaire extends Professeur {
+
+    public Titulaire(String nom, double salaire, String couleur) {
+        super(nom, salaire);
+        this.couleur = couleur;
+    }
+
+    public Titulaire(String nom, String couleur) {
+        super(nom);
+        this.couleur = couleur;
+    }
+
+    public  Titulaire(String couleur){
+        super();
+        this.couleur = couleur;
+    }
+    protected String couleur;
+
+    public String getCouleur(){return this.couleur;}
+
+    public void setCouleur(String couleur){this.couleur = couleur;}
+
+    public abstract void setCouleurDefaut();
+}
